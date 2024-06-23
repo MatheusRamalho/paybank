bc2f9bf85e03d5968c65a7e230e7bf513c6a3101
Matheus Ramalho Silva <matheusramalhosilva2@gmail.com>
2024-06-23 12:44:56 -0300

    feat(main): sessao de pessoas

--- src/app/(home)/modules/people.tsx
+++ src/app/(home)/modules/people.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 
 import { Section } from "@/components/Section";
+import { SessionTitle } from "@/components/SessionTitle";
 import { IconInstagram } from "@/components/Icons/IconInstagram";
 
 import img1 from '@/assets/imgs/img1-paybank.png'
@@ -11,48 +12,38 @@ export function HomePeople() {
     return (
         <Section id="section-4" legend="">
             <div className="">
-                <div className="grid-6">
-                    <div className="people-paybank p-3">
-                        <Image src={img1} alt="" />
-                    </div>
-                </div>
-
-                <div className="grid-6">
-                    <h6 className="uppercase color-gradient h7">pessoas reais</h6>
-
-                    <h3 className="mt-1">
-                        Por trás disso tudo, tem um objetivo:
-                        <span className="color-gradient">pessoas</span>
-                    </h3>
-
-                    <p className="mt-3">
-                        Lorem ipsum dolor sit amet consectetur. Et ut tempus
-                        facilisis tristique. Vitae consequat vel tortor aenean
-                        imperdiet posuere neque. Sollicitudin at at volutpat
-                        scelerisque ut aliquet velit tempor.
-                    </p>
-                </div>
-            </div>
-
-            <div className="">
-                <div className="grid-6">
-                    <div className="people-paybank p-3">
-                        <Image src={img2} alt="" />
-                    </div>
-
-                    <a href="#" className="btn-white flex-start-row mt-4 inline-block">
-                        <div className="flex-start-row">
-                            <IconInstagram />
-                            <h6 className="h7 mx-1">Instagram</h6>
+                <div className="flex">
+                    <div className="flex-1">
+                        <div className="relative block p-6 after:content-[''] after:z-[-1] after:absolute after:-top-[18px] after:left-[18px] after:size-4/5 after:rounded-xl after:border-2 after:border-secondary-700 after:skew-x-[-18deg] -after:transition after:duration-100 hover:after:transform-people-img">
+                            <Image src={img1} className="w-full" alt="" />
                         </div>
-                    </a>
+                    </div>
 
-                    <h4 className="mt-3">Por isso, nos siga no <br /> Instagram</h4>
+                    <div className="flex-1">
+                        <SessionTitle label="pessoas reais" title="Por trás disso tudo, tem um objetivo:" highlight="pessoas" description="Lorem ipsum dolor sit amet consectetur. Et ut tempus facilisis tristique. Vitae consequat vel tortor aenean imperdiet posuere neque. Sollicitudin at at volutpat scelerisque ut aliquet velit tempor." />
+                    </div>
                 </div>
 
-                <div className="grid-6">
-                    <div className="people-paybank p-2">
-                        <Image src={img3} alt="" />
+                <div className="flex mt-20">
+                    <div className="flex-1">
+                        <div className="relative block p-6 after:content-[''] after:z-[-1] after:absolute after:-top-[18px] after:left-[18px] after:size-4/5 after:rounded-xl after:border-2 after:border-secondary-700 after:skew-x-[-18deg] -after:transition after:duration-100 hover:after:transform-people-img">
+                            <Image src={img2} className="w-full" alt="" />
+                        </div>
+
+                        <a href="#" className="btn-white flex-start-row mt-4 inline-block">
+                            <div className="flex-start-row">
+                                <IconInstagram />
+                                <h6 className="h7 mx-1">Instagram</h6>
+                            </div>
+                        </a>
+
+                        <h4 className="mt-3">Por isso, nos siga no <br /> Instagram</h4>
+                    </div>
+
+                    <div className="flex-1">
+                        <div className="relative block p-6 after:content-[''] after:z-[-1] after:absolute after:-top-[18px] after:left-[18px] after:size-4/5 after:rounded-xl after:border-2 after:border-secondary-700 after:skew-x-[-18deg] -after:transition after:duration-100 hover:after:transform-people-img">
+                            <Image src={img3} className="w-full" alt="" />
+                        </div>
                     </div>
                 </div>
             </div>
--- src/components/SessionTitle/SessionTitle.tsx
+++ src/components/SessionTitle/SessionTitle.tsx
@@ -10,7 +10,7 @@ export function SessionTitle({ label, title, highlight, description }: SessionTi
         <div className="py-2 mb-6 flex flex-col gap-2">
             <h6 className="font-header font-medium text-lg uppercase leading-[140%] bg-gradient-g5 bg-clip-text text-transparent"> {label} </h6>
 
-            <h3 className="font-header font-normal text-7xl leading-[120%]">
+            <h3 className="font-header font-normal text-6xl leading-[120%]">
                 {title}
 
                 <span className="bg-gradient-g1 bg-clip-text text-transparent"> {highlight} </span>
