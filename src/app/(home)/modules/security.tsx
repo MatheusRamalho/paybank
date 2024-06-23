768bf799b84df6f87a83db5234099b67235576e9
Matheus Ramalho Silva <matheusramalhosilva2@gmail.com>
2024-06-23 14:07:29 -0300

    feat(main): estilizando a sesao de segurança

--- src/app/(home)/modules/security.tsx
+++ src/app/(home)/modules/security.tsx
@@ -20,53 +20,53 @@ export function HomeSecurity() {
             <div className="flex-1">
                 <SessionTitle label="investimentos" title="A sua segurança é nossa" highlight="prioridade" description="Sua segurança, confiabilidade, controle de suas finanças é o principal compromisso da Paybank." />
 
-                <div className="mt-3 pb-4 bb-gray hidden flex-start-row">
-                    <IconCircle className='icon-click'>
+                <div className="mt-6 pb-8 border-b-2 border-b-white-darken overflow-hidden flex flex-row items-center justify-start gap-2">
+                    <IconCircle variant="secondary" className='icon-click hover:cursor-pointer'>
                         <IconSmartphone />
                     </IconCircle>
 
-                    <IconCircle className='icon-click'>
+                    <IconCircle variant="secondary" className='icon-click hover:cursor-pointer'>
                         <IconFingerprint />
                     </IconCircle>
 
-                    <IconCircle className='icon-click'>
+                    <IconCircle variant="secondary" className='icon-click hover:cursor-pointer'>
                         <IconCard />
                     </IconCircle>
 
-                    <IconCircle className='icon-click'>
+                    <IconCircle variant="secondary" className='icon-click hover:cursor-pointer'>
                         <IconShield />
                     </IconCircle>
                 </div>
 
                 <div className="mt-3 relative min-h-44">
-                    <div className="text-click">
-                        <h4 className=""> Controle do cartão pelo App </h4>
-                        <p className="mt-2"> Perdeu o cartão ou suspeita de roubo? Bloqueie e desbloqueie o cartão pelo aplicativo em segundos.</p>
+                    <div className="text-click absolute top-0 left-0 transition duration-500">
+                        <h4 className="font-header font-normal text-3xl leading-[120%] text-black-light"> Controle do cartão pelo App </h4>
+                        <p className="mt-4 font-body text-base font-light leading-6 text-gray-7"> Perdeu o cartão ou suspeita de roubo? Bloqueie e desbloqueie o cartão pelo aplicativo em segundos.</p>
                     </div>
 
-                    <div className="text-click">
-                        <h4 className=""> Acesso protegido </h4>
-                        <p className="mt-2"> Uma camada extra de segurança para acessar o aplicativo com reconhecimento facial, impressão digital ou senha.</p>
+                    <div className="text-click absolute top-0 left-0 transition duration-500">
+                        <h4 className="font-header font-normal text-3xl leading-[120%] text-black-light"> Acesso protegido </h4>
+                        <p className="mt-4 font-body text-base font-light leading-6 text-gray-7"> Uma camada extra de segurança para acessar o aplicativo com reconhecimento facial, impressão digital ou senha.</p>
                     </div>
 
-                    <div className="text-click">
-                        <h4 className=""> Suporte </h4>
-                        <p className="mt-2"> Digite sua senha para confirmar transações e reporte qualquer movimentação não autorizada por você.</p>
+                    <div className="text-click absolute top-0 left-0 transition duration-500">
+                        <h4 className="font-header font-normal text-3xl leading-[120%] text-black-light"> Suporte </h4>
+                        <p className="mt-4 font-body text-base font-light leading-6 text-gray-7"> Digite sua senha para confirmar transações e reporte qualquer movimentação não autorizada por você.</p>
                     </div>
 
-                    <div className="text-click">
-                        <h4 className=""> Transações seguras </h4>
-                        <p className="mt-2"> Digite sua senha para confirmar transações e reporte qualquer movimentação não autorizada por você.</p>
+                    <div className="text-click absolute top-0 left-0 transition duration-500">
+                        <h4 className="font-header font-normal text-3xl leading-[120%] text-black-light"> Transações seguras </h4>
+                        <p className="mt-4 font-body text-base font-light leading-6 text-gray-7"> Digite sua senha para confirmar transações e reporte qualquer movimentação não autorizada por você.</p>
                     </div>
                 </div>
             </div>
 
-            <div className="flex-1 relative min-h-64">
-                <Image src={fig5} className="img-paybank" alt="" loading="lazy" />
-                <Image src={fig6} className="img-paybank" alt="" loading="lazy" />
-                <Image src={fig7} className="img-paybank" alt="" loading="lazy" />
-                <Image src={fig8} className="img-paybank" alt="" loading="lazy" />
-                <Image src={patterImg} className="pattern-paybank" alt="" loading="lazy" />
+            <div className="relative flex-1 min-h-64">
+                <Image src={fig5} className="img-paybank absolute top-0 left-0 transition duration-700 rounded-lg" alt="" loading="lazy" />
+                <Image src={fig6} className="img-paybank absolute top-0 left-0 transition duration-700 rounded-lg" alt="" loading="lazy" />
+                <Image src={fig7} className="img-paybank absolute top-0 left-0 transition duration-700 rounded-lg" alt="" loading="lazy" />
+                <Image src={fig8} className="img-paybank absolute top-0 left-0 transition duration-700 rounded-lg" alt="" loading="lazy" />
+                <Image src={patterImg} className="-z-[1] absolute -top-[40%] left-1/2 -translate-x-2/4 translate-y-[0%]" alt="" loading="lazy" />
             </div>
         </Section>
     )
